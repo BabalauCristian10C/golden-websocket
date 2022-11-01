44c30845df9c0fd27fba993b7b0d6e53d0210e5a
Chris <70657828+BabalauCristian10C@users.noreply.github.com>
2022-11-02 03:07:04 +0500

    Test Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+
+const PORT = 5000;
+
+const wsServer = new WebSocket.Server({
+    port: PORT
+});
+
+wsServer.on('connection', function (socket) {
+    console.log("A client just connected");
+
+    botUrl = undefined
+    socket.on('message', function (msg) {
+        console.log("Received message from client: "  + msg);
+        new_message = JSON.parse(msg)
+        if (new_message.botConnection){
+            botUrl = socket
+        } else {
+            if(new_message.bot){
+                try{
+                    botUrl.send("message to you") 
+                } catch {
+                    console.log("no bot link is set")
+                }
+            }
+        }
+
+        wsServer.clients.forEach(function (client) {
+            client.send("ss"+msg);
+        });
+
+    });
+
+});
+
+console.log( (new Date()) + " Server is listening on port " + PORT);
